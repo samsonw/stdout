23cc2ff98148ed9aa3c3aa922ec2b99b60810883
Samson <samson959@gmail.com>
2011-03-09 21:04:44 +0800

    highlight twitter account

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -2,4 +2,14 @@ module ActivitiesHelper
   def activity_count(model, page, count)
     model.class.per_page * (page.to_i - 1) + count + 1
   end
+  
+  def highlight_and_truncate(activity)
+    truncated_str = truncate_u(activity.content, 80, ' ...')
+    return truncated_str unless activity.source.name == 'twitter'
+    highlight(truncated_str)
+  end
+  
+  def highlight(str)
+    str.gsub(/(@\w+)/, '<span class="highlight">\1</span>')
+  end
 end

907765a57bcb70171710e0eefd83ff570f68b490
Samson Wu <samson959@gmail.com>
2011-03-11 21:41:25 +0800

    highlight github links

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -5,11 +5,21 @@ module ActivitiesHelper
   
   def highlight_and_truncate(activity)
     truncated_str = truncate_u(activity.content, 80, ' ...')
-    return truncated_str unless activity.source.name == 'twitter'
-    highlight(truncated_str)
+    case activity.source.name
+    when 'twitter'
+      highlight_twitter(truncated_str)
+    when 'github'
+      highlight_github(truncated_str)
+    else
+      truncated_str
+    end
   end
   
-  def highlight(str)
+  def highlight_twitter(str)
     str.gsub(/(@\w+)/, '<span class="highlight">\1</span>')
+  end
+  
+  def highlight_github(str)
+    str.gsub(/\[ (.*) \]/, '<span class="highlight_link"><a href="\1" target="_blank" title="github link">github link</a></span>')
   end
 end

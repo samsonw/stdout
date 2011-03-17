84ba9c6c2c53b341e66adb096b2555e1b3ce7f52
Samson Wu <samson959@gmail.com>
2011-03-17 19:57:52 +0800

    include source when fetching activities for performance

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
   
   def index
     @page = params[:page] || 1
-    @activities = Activity.paginate :page => @page, :order => 'publish_at DESC'
+    @activities = Activity.paginate :page => @page, :include => :source, :order => 'publish_at DESC'
   end
 
 end
